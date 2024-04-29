9ab40ff15f3bc6f9053e0ae4adf1ed0137262895
tauhidul-islam56 <tauhidulmahin@gmail.com>
2024-04-29 10:57:58 +0600

    initial commit

---
+++ country-json.js
@@ -0,0 +1,18 @@
+let country = [
+ 
+    {name: 'Afghanistan', code: 'AF'}, 
+    {name: 'Åland Islands', code: 'AX'}, 
+    {name: 'Albania', code: 'AL'}, 
+    {name: 'Algeria', code: 'DZ'}, 
+    {name: 'American Samoa', code: 'AS'}, 
+    {name: 'AndorrA', code: 'AD'}, 
+    {name: 'Angola', code: 'AO'}, 
+    {name: 'Anguilla', code: 'AI'}, 
+    {name: 'Antarctica', code: 'AQ'}, 
+    {name: 'Antigua and Barbuda', code: 'AG'}
+]    
+
+console.log(country[6].code);
+console.log(country[3].name);
+console.log(country[9].code);
+console.log(country[2].name);
